99aa95c04e131143c2bd6629197ada7023a634f1
Narelle Rocha <nanarocha14@hotmail.com>
2021-09-29 22:16:20 -0300

    Top bar quase pronta

---
+++ hogwarts/src/components/shared/AppTopBar.vue
@@ -0,0 +1,65 @@
+<template>
+    <div>
+        <v-app-bar
+            color="blue-grey darken-4"
+            app
+            dark
+            
+            >
+            <v-app-bar-nav-icon @click="drawer = true"></v-app-bar-nav-icon>
+
+            <v-toolbar-title>Hogwarts</v-toolbar-title>
+
+            <v-spacer></v-spacer>
+
+            <v-btn icon>
+                <v-icon>mdi-wizard-hat</v-icon>
+            </v-btn>
+
+            <v-btn icon>
+                <v-icon>mdi-auto-fix</v-icon>
+            </v-btn>
+
+        
+        </v-app-bar>
+        <v-navigation-drawer
+            v-model="drawer"
+            absolute
+            temporary
+        >
+        <v-list
+            nav
+            dense
+        >
+        <v-list-item-group
+          v-model="group"
+          active-class="deep-purple--text text--accent-4"
+        >
+          <v-list-item>
+            <v-list-item-icon>
+              <v-icon>mdi-home</v-icon>
+            </v-list-item-icon>
+            <v-list-item-title>Home</v-list-item-title>
+          </v-list-item>
+
+          <v-list-item>
+            <v-list-item-icon>
+              <v-icon>mdi-account</v-icon>
+            </v-list-item-icon>
+            <v-list-item-title>Account</v-list-item-title>
+          </v-list-item>
+        </v-list-item-group>
+      </v-list>
+    </v-navigation-drawer>
+  </div>
+</template>
+
+<script>
+export default {
+    name: 'App',
+    data: () => ({
+      drawer: false,
+      group: null,
+    }),
+}
+</script>
